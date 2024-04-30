e8d61f8a2aded3143ea138bef7bb1390c6eee94c
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-05-01 04:23:17 +0600

    create user function done

--- assets/php/actions.php
+++ assets/php/actions.php
@@ -3,7 +3,7 @@ require_once 'functions.php';
 if(isset($_GET['signup'])){
     $response = validateSignupForm($_POST);
     if($response['status']){
-        
+       
     }else{
         $_SESSION['error']=$response;
         $_SESSION['formdata'] = $_POST;
