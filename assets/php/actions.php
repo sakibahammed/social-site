7c86a35d719567c3f52f05a4ed9289254c38d214
Omar Ahammed Sakib <sakib.ahammed2345@gmail.com>
2024-05-01 05:28:04 +0600

    basic signup bugged

--- assets/php/actions.php
+++ assets/php/actions.php
@@ -3,7 +3,12 @@ require_once 'functions.php';
 if(isset($_GET['signup'])){
     $response = validateSignupForm($_POST);
     if($response['status']){
-       
+       if(createUser($_POST)){
+        header('location:../../?login');
+       }else{
+        echo "<script>alert('something is wrong')</alert>";
+       }
+
     }else{
         $_SESSION['error']=$response;
         $_SESSION['formdata'] = $_POST;
--- assets/php/functions.php
+++ assets/php/functions.php
@@ -1,135 +1,131 @@
+
+
 <?php
 
-require_once 'config.php';
-$db = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die("database is not connected");
+        require_once 'config.php';
+        $db = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME) or die("database is not connected");
 
 // function for showing pages 
 
 
-function showPage($page, $data = "")
-{
-    include("assets/pages/$page.php");
-}
+    function showPage($page,$data=""){
+        include("assets/pages/$page.php");
+    }
 
-//function for show errors.
+    //function for show errors.
 
-function showError($field)
-{
-    if (isset($_SESSION['error'])) {
-        $error = $_SESSION['error'];
-        if (isset($error['$field']) && $field == $error['field']) {
-?>
-            <div class="alert alert-danger my-2" role="alert">
-                <?= $error['msg'] ?>
-            </div>
+    function showError($field){
+        if(isset($_SESSION['error'])){
+            $error = $_SESSION['error'];
+            if(isset($error['$field']) && $field ==$error['field']){
+                ?>
+                    <div class="alert alert-danger my-2" role = "alert">
+                        <?=$error['msg']?>
+                    </div>
 
-<?php
+                <?php
+            }
         }
     }
-}
 
 
-//function for show previous form data
+    //function for show previous form data
 
-function showFormData($field)
-{
-    if (isset($_SESSION['formdata'])) {
-        $formdata = $_SESSION['formdata'];
-        return $formdata[$field];
-    }
-}
-
-
-// for checking duplicate email 
-
-
-function isEmailRegistered($email)
-{
-    global $db;
-    $query = "SELECT count(*) as row FROM users WHERE email='$email'";
-    $run = mysqli_query($db, $query);
-    $return_data = mysqli_fetch_assoc($run);
-    return $return_data['row'];
-}
-
-
-//checking duplicate username
-function isUsernameRegistered($username)
-{
-    global $db;
-    $query = "SELECT count(*) as row FROM users WHERE username='$username'";
-    $run = mysqli_query($db, $query);
-    $return_data = mysqli_fetch_assoc($run);
-    return $return_data['row'];
-}
-
-
-
-//creating user
-
-
-function createUser($data)
-{
-    global $db;
-    $first_name = mysqli_real_escape_string($db, $data['first_name']);
-    $last_name = mysqli_real_escape_string($db, $data['last_name']);
-    $gender = $data['gender'];
-    $email = mysqli_real_escape_string($db, $data['email']);
-    $username = mysqli_real_escape_string($db, $data['username']);
-    $password = mysqli_real_escape_string($db, $data['password']);
-    $password = md5($password);
-
-    $query = "INSERT INTO users(first_name,last_name,gender,email,username,password) ";
-    $query .= "VALUES ('$first_name','$last_name',$gender,'$email','$username','$password')";
-    return mysqli_query($db, $query);
-}
-
-
-
-// validating the signup form
-
-function validateSignupForm($form_data)
-{
-    $response = array();
-    $response['status'] = true;
-    if (!$form_data['password']) {
-        $response['msg'] = "password is not given";
-        $response['status'] = false;
-        $response['field'] = 'password';
-    }
-    if (!$form_data['username']) {
-        $response['msg'] = "username is not given";
-        $response['status'] = false;
-        $response['field'] = 'username';
-    }
-    if (!$form_data['email']) {
-        $response['msg'] = "email is not given";
-        $response['status'] = false;
-        $response['field'] = 'email';
-    }
-    if (!$form_data['last_name']) {
-        $response['msg'] = "last name is not given";
-        $response['status'] = false;
-        $response['field'] = 'last_name';
-    }
-    if (!$form_data['first_name']) {
-        $response['msg'] = "first name is not given";
-        $response['status'] = false;
-        $response['field'] = 'first_name';
+    function showFormData($field){
+        if(isset($_SESSION['formdata'])){
+            $formdata = $_SESSION['formdata'];
+            return $formdata[$field];
+        }
     }
 
 
-    if (isEmailRegistered($form_data['email'])) {
-        $response['msg'] = "email id is already registered";
-        $response['status'] = false;
-        $response['field'] = 'email';
+    // for checking duplicate email 
+
+
+    function isEmailRegistered($email){
+        global $db;
+        $query="SELECT count(*) as row FROM users WHERE email='$email'";
+        $run=mysqli_query($db,$query);
+        $return_data = mysqli_fetch_assoc($run);
+        return $return_data['row'];
     }
-    if (isUsernameRegistered($form_data['username'])) {
-        $response['msg'] = "username is already registered";
-        $response['status'] = false;
-        $response['field'] = 'username';
+
+
+    //checking duplicate username
+    function isUsernameRegistered($username){
+        global $db;
+        $query="SELECT count(*) as row FROM users WHERE username='$username'";
+        $run=mysqli_query($db,$query);
+        $return_data = mysqli_fetch_assoc($run);
+        return $return_data['row'];
     }
-    return $response;
-}
+
+    
+
+    //creating user
+
+
+    function createUser($data){
+        global $db;
+        $first_name = mysqli_real_escape_string($db,$data['first_name']);
+        $last_name = mysqli_real_escape_string($db,$data['first_name']);
+        $gender = $data['gender'];
+        $first_name = mysqli_real_escape_string($db,$data['first_name']);
+        $first_name = mysqli_real_escape_string($db,$data['first_name']);
+        $first_name = mysqli_real_escape_string($db,$data['first_name']);
+        $query = "INSERT INTP users(first_name,last_name,gender,email,username,password)";
+        $query .= "VALUES ()";
+
+    }
+
+
+
+    // validating the signup form
+
+    function validateSignupForm($form_data){
+        $response = array();
+        $response['status']=true;
+        if(!$form_data['password']){
+            $response['msg']="password is not given" ;
+            $response['status'] = false;
+            $response['field']='password';
+        }
+        if(!$form_data['username']){
+            $response['msg']="username is not given" ;
+            $response['status'] = false;
+            $response['field']='username';
+        }
+        if(!$form_data['email']){
+            $response['msg']="email is not given" ;
+            $response['status'] = false;
+            $response['field']='email';
+        }
+        if(!$form_data['last_name']){
+            $response['msg']="last name is not given" ;
+            $response['status'] = false;
+            $response['field']='last_name';
+        }
+        if(!$form_data['first_name']){
+            $response['msg']="first name is not given" ;
+            $response['status'] = false;
+            $response['field']='first_name';
+        }
+
+
+        if(isEmailRegistered($form_data['email'])){
+            $response['msg']="email id is already registered";
+            $response['status']=false;
+            $response['field']='email';
+        }
+        if(isUsernameRegistered($form_data['username'])){
+            $response['msg']="username is already registered";
+            $response['status']=false;
+            $response['field']='username';
+        }
+        return $response;
+      
+       
+       
+    }
 
 ?>
